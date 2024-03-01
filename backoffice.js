86bd7e3a2d36ba4662fa3053b34023cba47e0019
ALFIO BATTIATO <battiato3@gmail.com>
2024-03-01 21:08:29 +0100

    tasto modifica

--- backoffice.js
+++ backoffice.js
@@ -1,36 +1,88 @@
 const apiKey =
-"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWUxZDNhZDRjNTllYzAwMTk5MGQ4ZGYiLCJpYXQiOjE3MDkyOTg2MDUsImV4cCI6MTcxMDUwODIwNX0.mb1tywt7mUK6KjJ7LSC14VY6TgMaADn0jFNfPfzBsKI";
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWUxZDNhZDRjNTllYzAwMTk5MGQ4ZGYiLCJpYXQiOjE3MDkyOTg2MDUsImV4cCI6MTcxMDUwODIwNX0.mb1tywt7mUK6KjJ7LSC14VY6TgMaADn0jFNfPfzBsKI";
 const url = "https://striveschool-api.herokuapp.com/api/product/";
-  const form = document.querySelector("form");
-  form.addEventListener("submit", function (event) {
-    event.preventDefault(); 
-    postData();
-  });
-function postData() {
-  const titolo = document.getElementById("Titolo").value;
-  const descrizione = document.getElementById("Descrizione").value;
-  const brand = document.getElementById("brand").value;
-  const imgUrl = document.getElementById("img").value;
-  const prezzo = document.getElementById("prezzo").value;
+const form = document.querySelector("form");
+const btnSubmit = document.getElementsByClassName("btn-primary")[0];
+const titolo = document.getElementById("Titolo");
+const descrizione = document.getElementById("Descrizione");
+const brand = document.getElementById("brand");
+const imgUrl = document.getElementById("img");
+const prezzo = document.getElementById("prezzo");
 
-  const data = {
-    name:titolo,
-    description: descrizione,
-    brand: brand,
-    imageUrl: imgUrl,
-    price: prezzo,
-  };
+const id = new URLSearchParams(window.location.search).get("idProdotto");
 
-  fetch(url, {
-    method: "POST", // è come scrivere method: method,
-    body: JSON.stringify(data), // è fondamentale fare la stringhifizzazione dell'oggetto nativo o invieremo "[object Object]"
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////qua siamo nella modalità modifica
+if (id !== null) {
+  btnSubmit.className = "btn btn-success";
+  btnSubmit.textContent = "Modifica";
+
+  fetch(url + id, {
+    method: "GET",
     headers: {
       Authorization: apiKey,
       "Content-Type": "application/json",
     },
   })
     .then((response) => {
-      console.log(response);
+      if (response.ok) {
+        return response.json();
+      } else {
+        if (response.status === 400) {
+          throw new Error("400 - Errore lato client");
+        }
+        if (response.status === 404) {
+          throw new Error("404 - Dato non trovato");
+        }
+        if (response.status === 500) {
+          throw new Error("500 - Errore lato server");
+        }
+        throw new Error("Errore nel reperimento dati");
+      }
+    })
+    .then((product) => {
+      console.log(product);
+      document.getElementById("Titolo").value = product.name;
+      document.getElementById("Descrizione").value = product.description;
+      document.getElementById("brand").value = product.brand;
+      document.getElementById("img").value = product.imageUrl;
+      document.getElementById("prezzo").value = product.price;
+    })
+    .catch((err) => console.log(err));
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    putFetch(id);
+  });
+  btnSubmit.onclick = function () {};
+  ////////////////////////////////////////modalità creazione
+} else {
+  btnSubmit.className = "btn btn-primary";
+  btnSubmit.textContent = "Submit";
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    postData();
+  });
+}
+
+function postData() {
+
+
+  const data = {
+    name: titolo.value,
+    description: descrizione.value,
+    brand: brand.value,
+    imageUrl: imgUrl.value,
+    price: prezzo.value,
+  };
+
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      Authorization: apiKey,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
@@ -47,10 +99,56 @@ function postData() {
       }
     })
     .then((newAppointment) => {
-      
-        alert("Auto è stata inserita con successo ");
-        e.target.reset();
-      
+      alert("Appuntamento con id: " + " è stato creato correttamente");
+      form.reset();
+    })
+    .catch((err) => console.log(err));
+}
+
+function putFetch(id) {
+  
+
+  const data = {
+    name: titolo.value,
+    description: descrizione.value,
+    brand: brand.value,
+    imageUrl: imgUrl.value,
+    price: prezzo.value,
+  };
+
+  fetch(url + id, {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      Authorization: apiKey,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        if (response.status === 400) {
+          throw new Error("400 - Errore lato client");
+        }
+        if (response.status === 404) {
+          throw new Error("404 - Dato non trovato");
+        }
+        if (response.status === 500) {
+          throw new Error("500 - Errore lato server");
+        }
+        throw new Error("Errore nel reperimento dati");
+      }
+    })
+    .then((updatedProduct) => {
+      console.log(updatedProduct);
+      document.getElementById("Titolo").value = updatedProduct.name;
+      document.getElementById("Descrizione").value = updatedProduct.description;
+      document.getElementById("brand").value = updatedProduct.brand;
+      document.getElementById("img").value = updatedProduct.imageUrl;
+      document.getElementById("prezzo").value = updatedProduct.price;
+      alert("Annuncio è stato modificato correttamente");
+
     })
     .catch((err) => console.log(err));
 }
--- index.js
+++ index.js
@@ -3,70 +3,54 @@ const row = document.getElementsByClassName("row")[0];
 const apiKey =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWUxZDNhZDRjNTllYzAwMTk5MGQ4ZGYiLCJpYXQiOjE3MDkyOTg2MDUsImV4cCI6MTcxMDUwODIwNX0.mb1tywt7mUK6KjJ7LSC14VY6TgMaADn0jFNfPfzBsKI";
   
-  const params = new URLSearchParams(window.location.search) // oggetto costruito a partire dai parametri nella URL es. ?agendaId=2938123
-  
-  const appointmentId = params.get("agendaId") // metodo sull'oggetto URLSearchParams che ci estrae il valore corrispondente alla chiave "agendaId" da noi scelta e applicata al link in homepage
-  
-  console.log("RESOURCE ID:", appointmentId)
-  
-  const url = "https://striveschool-api.herokuapp.com/api/product/";
+const url = "https://striveschool-api.herokuapp.com/api/product/";
 
-
-
-
-function creaCard(immagine, title, descrizione,id) {
-  // ///////////////////////////////////////
+function creaCard(immagine, title, descrizione, id) {
   const col = document.createElement("div");
-  row.appendChild(col);
   col.className = "col-6 col-md-3";
 
-  // ///////////////////////////////////////
   const card = document.createElement("div");
-  col.appendChild(card);
   card.className = "card";
-  card.style.height="22rem"
-  // ///////////////////////////////////////
+  card.style.height = "22rem";
+
   const img = document.createElement("img");
-  card.appendChild(img);
   img.className = "card-img-top object-fit-cover";
   img.src = immagine;
-  img.style.height="60%";
+  img.style.height = "60%";
 
-  // ///////////////////////////////////////
   const body = document.createElement("div");
-  card.appendChild(body);
   body.className = "card-body";
 
-  // ///////////////////////////////////////
   const h5 = document.createElement("h5");
-  body.appendChild(h5);
   h5.className = "card-title";
   h5.textContent = title;
 
-  // ///////////////////////////////////////
   const p = document.createElement("p");
-  body.appendChild(p);
   p.className = "card-text";
   p.textContent = descrizione;
 
-  // ///////////////////////////////////////
   const btnDettaglio = document.createElement("a");
-  body.appendChild(btnDettaglio);
   btnDettaglio.href = `./dettagli.html?idProdotto=${id}`;
   btnDettaglio.className = "btn btn-primary me-1";
   btnDettaglio.innerText = "Info";
 
-  // ///////////////////////////////////////
   const btnModifica = document.createElement("a");
-  body.appendChild(btnModifica);
-  btnModifica.href = "./backoffice.html";
+  btnModifica.href = `./backoffice.html?idProdotto=${id}`;
   btnModifica.className = "btn btn-success me-1";
   btnModifica.innerText = "Modifica";
+
+  row.appendChild(col);
+  col.appendChild(card);
+  card.appendChild(img);
+  card.appendChild(body);
+  body.appendChild(h5);
+  body.appendChild(p);
+  body.appendChild(btnDettaglio);
+  body.appendChild(btnModifica);
 }
 
-
 fetch(url, {
-  method: "GET", // è come scrivere method: method,
+  method: "GET",
   headers: {
     Authorization: apiKey,
     "Content-Type": "application/json",
@@ -89,12 +73,8 @@ fetch(url, {
     }
   })
   .then((newAppointment) => {
-    console.log(newAppointment);
-
     newAppointment.forEach((oggetto) => {
-      creaCard(oggetto.imageUrl, oggetto.name, oggetto.description,oggetto._id);
-     
+      creaCard(oggetto.imageUrl, oggetto.name, oggetto.description, oggetto._id);
     });
   })
   .catch((err) => console.log(err));
-
